01f312a752bb9f82ddc286eb9d5872460a9e4953
Im1x <im1xdev@gmail.com>
2024-11-28 18:58:27 +0300

    Fix🛠️: Fix field type for storing timestamp

--- src-tauri/src/models/response.rs
+++ src-tauri/src/models/response.rs
@@ -3,13 +3,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Task {
-    id: u32,
+    id: u64,
     title: String,
     time_end: u32,
     time: u32,
     is_paused: bool,
     is_cyclic: bool,
     is_completed: bool,
-    sound: String
+    sound: String,
 }
-
